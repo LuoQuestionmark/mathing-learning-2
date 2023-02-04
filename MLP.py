234372b31c9e09ce904dd5a73798a3bb42e889e8
Wenhao LUO <Wenhao.Luo@telecomnancy.eu>
2023-02-04 16:40:30 -0500

    update forward function so that now a list of weights can be used

--- MLP.py
+++ MLP.py
@@ -117,40 +117,71 @@ class NeuralNetMLP(object):
             raise AttributeError('`how` must be `column` or `row`')
         return X_new
 
-    def _feedforward(self, X, w1, w2):
+    def _del_bias_unit(self, X, how='column'):
+        """Del bias unit (column or row of 1s) to array at index 0"""
+        if how == 'column':
+            X_new = np.ones((X.shape[0], X.shape[1] - 1))
+            X_new = X[:, 1:]
+        elif how == 'row':
+            X_new = np.ones((X.shape[0] + 1, X.shape[1]))
+            X_new = X[1:, :]
+        else:
+            raise AttributeError('`how` must be `column` or `row`')
+        return X_new        
+
+    def _feedforward(self, X, weights):
         """Compute feedforward step
 
         Parameters
         -----------
         X : array, shape = [n_samples, n_features]
             Input layer with original features.
-        w1 : array, shape = [n_hidden_units, n_features]
-            Weight matrix for input layer -> hidden layer.
-        w2 : array, shape = [n_output_units, n_hidden_units]
-            Weight matrix for hidden layer -> output layer.
+
+        weights: an Iterable of weights
+        # w1 : array, shape = [n_hidden_units, n_features]
+        #     Weight matrix for input layer -> hidden layer.
+        # w2 : array, shape = [n_output_units, n_hidden_units]
+        #     Weight matrix for hidden layer -> output layer.
 
         Returns
         ----------
-        a1 : array, shape = [n_samples, n_features+1]
-            Input values with bias unit.
-        z2 : array, shape = [n_hidden, n_samples]
-            Net input of hidden layer.
-        a2 : array, shape = [n_hidden+1, n_samples]
-            Activation of hidden layer.
-        z3 : array, shape = [n_output_units, n_samples]
-            Net input of output layer.
-        a3 : array, shape = [n_output_units, n_samples]
-            Activation of output layer.
+        ax: iterable of a1, a2, ..., an
+        zx: iterable of z2, z3, ..., zn
+        # a1 : array, shape = [n_samples, n_features+1]
+        #     Input values with bias unit.
+        # a2 : array, shape = [n_hidden+1, n_samples]
+        #     Activation of hidden layer.
+        # a3 : array, shape = [n_output_units, n_samples]
+        #     Activation of output layer.
+        # z2 : array, shape = [n_hidden, n_samples]
+        #     Net input of hidden layer.
+        # z3 : array, shape = [n_output_units, n_samples]
+        #     Net input of output layer.
 
         """
         a1 = self._add_bias_unit(X, how='column')
-        z2 = w1.dot(a1.T)
-        a2 = self._sigmoid(z2)
-        a2 = self._add_bias_unit(a2, how='row')
-        z3 = w2.dot(a2)
-        a3 = self._sigmoid(z3)
+        ax = list()
+        zx = list()
+        ax.append(a1)
+
+        a_temp = a1.T
+        for w in weights:
+            z_temp = w.dot(a_temp)
+            a_temp = self._sigmoid(z_temp)
+            a_temp = self._add_bias_unit(a_temp, how='row')
+            ax.append(a_temp)
+            zx.append(z_temp)
+
+        # z2 = w1.dot(a1.T)
+        # a2 = self._sigmoid(z2)
+        # a2 = self._add_bias_unit(a2, how='row')
+        # z3 = w2.dot(a2)
+        # a3 = self._sigmoid(z3)
         # return a1, z2, a2, z3, a3
-        return [a1, a2, a3], [z2, z3]
+        a_end = ax.pop()
+        a_end = self._del_bias_unit(a_end, how='row')
+        ax.append(a_end)
+        return ax, zx
 
     def _L2_reg(self, lambda_, w1, w2):
         """Compute L2-regularization cost"""
@@ -256,7 +287,7 @@ class NeuralNetMLP(object):
                                  'Use X[:,None] for 1-feature classification,'
                                  '\nor X[[i]] for 1-sample classification')
 
-        [a1, a2, a3], [z2, z3] = self._feedforward(X, self.w1, self.w2)
+        [a1, a2, a3], [z2, z3] = self._feedforward(X, [self.w1, self.w2])
         y_pred = np.argmax(z3, axis=0)
         return y_pred
 
@@ -305,9 +336,8 @@ class NeuralNetMLP(object):
                                   self.minibatches)  # Si le mode minibatch est activé, le dataset en entrée est divisé en batch pour le calcul des gradients
             for idx in mini:
                 # feedforward
-                # a1, z2, a2, z3, a3 = self._feedforward(X_data[idx], self.w1,
-                #                                        self.w2)  # Ce que nous avons vu jusqu'à présent
-                [a1, a2, a3], [z2, z3] = self._feedforward(X_data[idx], self.w1, self.w2) 
+
+                [a1, a2, a3], [z2, z3] = self._feedforward(X_data[idx], [self.w1, self.w2]) 
 
                 cost = self._get_cost(y_enc=y_enc[:, idx], output=a3, w1=self.w1, w2=self.w2)
                 self.cost_.append(cost)
